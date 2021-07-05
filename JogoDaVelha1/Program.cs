8bebd902c6b64841c6dd608663f0de1b390b85b8
galemagalhaes <g.alemagalhaes@gmail.com>
2021-07-05 13:02:24 -0300

    Tabuleiro
    Criação do tabuleiro (matriz) do jogo da velha no console

---
+++ JogoDaVelha1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JogoDaVelha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] matriz = new string[3, 3];
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = "0";
+                }
+            }
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"[{matriz[i, j]}]");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
+    
+
